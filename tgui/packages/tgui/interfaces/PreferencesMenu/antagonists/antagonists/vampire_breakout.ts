35517624696218ec256109bcfbf4728f55a1fd8d
Lucy <lucy@absolucy.moe>
2026-03-04 22:48:52 -0500

    prefs menu categories

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/vampire_breakout.ts
@@ -0,0 +1,17 @@
+// THIS IS A OCULIS UI FILE
+import { type Antagonist, Category } from '../base';
+
+const VampireBreakout: Antagonist = {
+  key: 'vampire_breakout',
+  name: 'Vampire Breakout',
+  description: [
+    `
+      After your death, you awaken to see yourself as an undead monster.
+      Use your Vampiric abilities as best you can.
+      Scrape by Space Station 13, or take over it, vassalizing your way.
+    `,
+  ],
+  category: Category.Latejoin,
+};
+
+export default VampireBreakout;
---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/vampiricaccident.ts
@@ -0,0 +1,17 @@
+// THIS IS A OCULIS UI FILE
+import { type Antagonist, Category } from '../base';
+
+const VampiricAccident: Antagonist = {
+  key: 'vampiric_accident',
+  name: 'Vampiric Accident',
+  description: [
+    `
+      After your death, you awaken to see yourself as an undead monster.
+      Use your Vampiric abilities as best you can.
+      Scrape by Space Station 13, or take over it, vassalizing your way.
+    `,
+  ],
+  category: Category.Midround,
+};
+
+export default VampiricAccident;
